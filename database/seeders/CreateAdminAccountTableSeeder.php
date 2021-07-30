7db06e4c57c811fe4086e784a81e179796f0fa30
Danilo De La Cruz <daklo.info@gmail.com>
2021-07-30 02:40:36 -0400

    Adding admin user seeder

---
+++ database/seeders/CreateAdminAccountTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CreateAdminAccountTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+	    if (!User::whereEmail('admin@videostats.com')->first()) {
+		    User::create([
+			    'name' => 'Administrator',
+			    'email' => 'admin@videostats.com',
+			    'email_verified_at' => now(),
+			    'password' => \Hash::make('video@stats2021'),
+			    'role' => 'admin',
+			    'remember_token' => Str::random(10),
+		    ]);
+	    }
+    }
+}
